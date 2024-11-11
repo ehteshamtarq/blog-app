f4fc85372cab00ae20374b1b411aa982a4edb28c
EHTESHAM TARIQUE <tariqueehtesham1@gmail.com>
2024-11-11 10:38:35 +0530

    user registration form

---
+++ blog_app/users/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# class meta gives us a nested namespace for configurations and keeps the configurations in one place
+# Within the configuration we are saying that the model that will be affected is the user model so when 
+# you do form.save() it is going to save to User model and fields which are there in the list that we want in the form in that order
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        
