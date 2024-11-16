ac9ce090c324b29f7efd173be546045a269adfdc
EHTESHAM TARIQUE <tariqueehtesham1@gmail.com>
2024-11-16 09:48:28 +0530

    update User Profile

--- blog_app/users/forms.py
+++ blog_app/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 # class meta gives us a nested namespace for configurations and keeps the configurations in one place
 # Within the configuration we are saying that the model that will be affected is the user model so when 
 # you do form.save() it is going to save to User model and fields which are there in the list that we want in the form in that order
@@ -12,4 +12,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-        
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
