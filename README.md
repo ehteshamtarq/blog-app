b10babca9361ff3baee033bfb77e1e9c6a38fb24
EHTESHAM TARIQUE <tariqueehtesham1@gmail.com>
2024-11-18 00:15:37 +0530

    email and password reset

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # blog-app
+
+## FUNCTIONALITY
+
+- User Registration
+- Login Logout Authentication
+- Update User Profile
+- Create, Update, Delete Posts
+- Pagination
+- Filter Posts By User
+- Password Reset
--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 import os
 """
 Django settings for blog_app project.
@@ -15,6 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv() 
 
 
 # Quick-start development settings - unsuitable for production
@@ -137,3 +139,11 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
